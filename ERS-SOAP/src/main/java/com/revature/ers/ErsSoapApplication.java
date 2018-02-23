d06b0aa1c82ba2109c0a017573b0d8a69068ba9e
Allan <allanjpoindexter@outlook.com>
2018-02-23 13:45:51 -0500

    Initial Commit

---
+++ ERS-SOAP/src/main/java/com/revature/ers/ErsSoapApplication.java
@@ -0,0 +1,12 @@
+package com.revature.ers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErsSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErsSoapApplication.class, args);
+	}
+}
